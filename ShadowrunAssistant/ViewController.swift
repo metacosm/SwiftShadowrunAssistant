c7e1a0d0ea1f794d24a2cc1298e7a90f26f4f9a0
Chris Laprun <metacosm@gmail.com>
2016-12-14 14:44:00 +0100

    Initial Commit

---
+++ ShadowrunAssistant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShadowrunAssistant
+//
+//  Created by Christophe Laprun on 14/12/2016.
+//  Copyright © 2016 Christophe Laprun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
